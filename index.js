407f4eb3662095784e0ae9fa9bd7c77b982b7692
Felipe Gonzalez <fgonzalezcare@gmail.com>
2023-03-15 22:38:39 -0700

    add endpoints for get and post, also app is working with the backend

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const axios = require('axios');
 const { v4: uuid } = require("uuid");
+const videoRoutes = require('./routes/video.js');
+const cors = require('cors');
 
-const videoData = require('./routes/video.js');
 const app = express();
-const PORT = 3000;
-
-const api_key = 'f9533bff-7349-4e6e-85ee-34094bc38c36';
-const videosUrl = `https://project-2-api.herokuapp.com/videos/?api_key=${api_key}`;
-
+const port = 3001;
 
 app.use(express.json());
-app.use('/videos', videoData);
+app.use(cors());
+
+app.use('/', videoRoutes);
 
 app.get(`/`, (req, res) => res.send('Hello from homepage'));
 
-app.listen(PORT, () => console.log(`Sever running on port ${PORT}`));
-
-
+app.listen(port, () => console.log(`Sever running on port ${port}`));
--- routes/video.js
+++ routes/video.js
@@ -1,20 +1,40 @@
 const express = require('express');
 const router = express.Router();
-const videoDetails = require('../data/video-details.json');
-
+const videoData = require('../data/videos.json');
+const uuid = require("uuid");
 
 router.get('/', (req, res) => {
-    console.log(videoDetails);
-    res.send('Hello');
+    const videoList = videoData.map(video => {
+      return {
+        id: video.id,
+        title: video.title,
+        channel: video.channel,
+        image: video.image
+      };
+    });
+    console.log('Getting response from API');
+    res.send(videoList);
+  });
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const video = videoData.find((video) => video.id === id);
+    if (video){
+        res.json(video);
+    } else {
+        res.status(404).send('Video not found')
+    }
 });
 
-
-router.post('/', (req, res) =>{
-   console.log('Recieved');
-   const video = req.body;
-
-   videoDetails.push(video);
-   res.send('Reached');
-})
+router.post('/', (req, res) => {
+    const newVideo = {
+      id: uuid(),
+      title: req.body.title,
+      channel: req.body.channel,
+      image: req.body.image
+    }
+    videoData.push(newVideo);
+    res.status(201).json(videoData)
+  })
 
 module.exports = router
